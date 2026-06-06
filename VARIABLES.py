6bc98293bc5b8da8e018a35cb8e7ae27baf7fce1
OliverCantillano <Oliviercantillano@gmail.com>
2026-06-06 15:28:42 -0600

    Hopefully works

---
+++ VARIABLES.py
@@ -0,0 +1,27 @@
+# VARIABLES IN PYTHON
+
+# String    ///  The string have to be with "" or ''
+email = 'jimmy.f.cantillano@gmail.com'
+print(f'Your Email Is {email}')
+
+# Integers   /// They are just numbers
+
+age = 22
+print(f'Your Age is {age} years old ')
+
+
+# Float    /// They are integers but with decimals
+
+price = 10.99
+print(f'The product price is {price}')
+
+# Boolean      ///  True  or   False  //
+# Here we can use 'IF' or 'ELSE' if it is anything else
+# At 'if' and 'else' we end the code with ':', if is the only one we select for true or false
+
+is_student = True
+
+if is_student:
+    print('Your are a very successful student ')
+else:
+    print('Your are not a successful student ')
